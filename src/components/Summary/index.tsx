e8ed9da554a1fcaa0374c20c7c8fb753cdeef366
nittosantos <nittosantos.dev@gmail.com>
2024-01-25 12:07:08 -0300

    feat: :sparkles: add a hook useSummary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,30 +1,10 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { priceFromatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "incoming") {
-        acc.incoming += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcoming += transaction.price;
-        acc.total -= transaction.price;
-      }
-      return acc;
-    },
-    {
-      incoming: 0,
-      outcoming: 0,
-      total: 0,
-    }
-  );
-
+  const { incoming, outcoming, total } = useSummary();
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -33,7 +13,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{priceFromatter.format(summary.incoming)}</strong>
+        <strong>{priceFromatter.format(incoming)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -42,16 +22,16 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{priceFromatter.format(summary.outcoming)}</strong>
+        <strong>{priceFromatter.format(outcoming)}</strong>
       </SummaryCard>
 
-      <SummaryCard variant={summary.total > 0 ? "green" : "red"}>
+      <SummaryCard variant={total > 0 ? "green" : "red"}>
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{priceFromatter.format(summary.total)}</strong>
+        <strong>{priceFromatter.format(total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
